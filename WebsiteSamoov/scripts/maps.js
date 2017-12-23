95054a5fa4e712eefdb17bfd18550ee64b330796
JefGeeraerts <32387101+JefGeeraerts@users.noreply.github.com>
2017-12-23 12:31:23 +0100

    Add files via upload

---
+++ WebsiteSamoov/scripts/maps.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 50.931102, lng: 4.430787};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
